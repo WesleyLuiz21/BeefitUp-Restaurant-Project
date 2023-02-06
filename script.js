a73cd4ef73422975404dd446c24fdc569493b972
Wesley <84082753+WesleyLuiz21@users.noreply.github.com>
2023-02-06 02:46:47 +0000

    change the scroll amount in the script js and fixed the hamburguer menu right margin

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const header = document.getElementById('header'); // gets the header element
 
 // if user scroll more than 40 from the top it will change the background color to black
 function scrollColor() {
-    if(document.body.scrollTop > 40 ||  document.documentElement.scrollTop > 40) { 
+    if(document.body.scrollTop > 20 ||  document.documentElement.scrollTop > 20) { 
         header.style.background = '#0c0c0c';
     } else {
         header.style.background = 'none'; // if the user scrolls back to the top, it reverts to the initial state or transparent background
